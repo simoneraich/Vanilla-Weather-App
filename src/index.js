09203445099a9063b0677b55e159548db44e4004
simoneraich <office@simoneraich.com>
2022-09-24 17:01:14 +0200

    moving HTML in JS and setting up API

--- src/index.js
+++ src/index.js
@@ -24,6 +24,38 @@ function formatedTime(date) {
 
   return `${days[dayIndex]} ${hours}:${minutes}`;
 }
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `     <div class="col-2">
+            <div class="forecast-day">${day}</div> 
+            <img src="media/sun_clouds.png" alt="icon"/>
+            <div class="forecast-temp">
+              <span class="forecast-max">
+                  18°
+              </span>
+              <span class="forecast-min">
+                12°
+              </span>
+            </div>
+          </div>
+       `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function showTemperature(response) {
   document.querySelector("#current-city").innerHTML = response.data.name;
@@ -43,6 +75,8 @@ function showTemperature(response) {
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
   celciusTemp = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function searchingCity(city) {
