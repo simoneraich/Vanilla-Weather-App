afb5c29b4fca4e9db0c43153c080f2eb676fceba
simoneraich <office@simoneraich.com>
2022-09-24 18:06:36 +0200

    integrated API

--- src/index.js
+++ src/index.js
@@ -24,27 +24,38 @@ function formatedTime(date) {
 
   return `${days[dayIndex]} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `     <div class="col-2">
-            <div class="forecast-day">${day}</div> 
-            <img src="media/sun_clouds.png" alt="icon"/>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `     <div class="col-2">
+            <div class="forecast-day">${formatDay(forecastDay.dt)}</div> 
+            <img src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png" alt="icon"/>
             <div class="forecast-temp">
               <span class="forecast-max">
-                  18°
+                ${Math.round(forecastDay.temp.max)}°
               </span>
               <span class="forecast-min">
-                12°
+                ${Math.round(forecastDay.temp.min)}°
               </span>
             </div>
           </div>
        `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
